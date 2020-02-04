dcfacc0052f31a52d56992d35a32372380534b5f
Camille Bouzerand <camille.bouzerand@epitech.eu>
2020-02-04 15:48:32 +0100

    [ADD] first part of cgi module on local simple fork

--- src/modules/php_cgi/php_module.cpp
+++ src/modules/php_cgi/php_module.cpp
@@ -5,15 +5,67 @@
 ** 
 */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <iostream>
 
 int main(int argc, char **argv, char **env)
 {
-    const char *bin = "/usr/bin/php-cgi";
+    int pipefd[2];
+    pid_t pid;
+    char buf;
 
-    int rc = execve(
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {    // Le fils
+        const char *bin = "/usr/bin/php-cgi";
+        // const char *argv[] = {bin, NULL};
+        close(pipefd[0]);   // Ferme l'extrémité de lecture inutilisée
+        if (dup2(pipefd[1], 1) == -1) {
+            std::cout << "dup2 a echoue" << std::endl;
+            exit(84);
+        }
+
+        int rc = execve(
             bin,
             const_cast<char * const *>(argv),
             const_cast<char * const *>(env)
-        );
+            );
+
+        _exit(rc);
+
+    } else {
+        close(pipefd[1]);
+        int exitCode = 0;
+        waitpid(pid, &exitCode, 0);
+        if (exitCode != 0) {
+            std::cout << "hum l'execve a pas marché" << std::endl;
+            exit(84);
+        }
+
+        int r = -1;
+        char buff[128];
+        std::string content;
+        while ((r = read(pipefd[0], buff, 128)) != -1 && r > 0)
+            content.append(buff, r);
+        close(pipefd[0]);
+        if (r == -1) {
+            std::cout << "hum pb de read" << std::endl;
+            exit(84);
+        }
+        std::cout << "le content final: " << std::endl << content << std::endl;
+    }
+    return (0);
 }
