c5d8550109ef4ae2adf7461b42d905f4b39c6100
Théo Dedieu <theo.dedieu@epitech.eu>
2020-02-15 16:00:04 +0100

    [ADD]: CMakeLists.txt to compile 'parser' shared library

--- src/server/modules/Hello.cpp
+++
@@ -1,4 +0,0 @@
-/* --- Hello.cpp --- */
-#include "Hello.hpp"
-
-extern "C" oZ::IModule *CreateModule(void) { return new Hello(); }
--- src/server/modules/World.cpp
+++
@@ -1,4 +0,0 @@
-/* --- World.cpp --- */
-#include "World.hpp"
-
-extern "C" oZ::IModule *CreateModule(void) { return new World(); }
