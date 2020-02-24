39a53c243a1b2fdbc1b3b9a0572f820984760cac
Camille Bouzerand <camille.bouzerand@epitech.eu>
2020-02-24 20:43:53 +0100

    first push

--- src/modules/php_cgi/php_module.cpp
+++ src/modules/php_cgi/php_module.cpp
@@ -5,13 +5,9 @@
 ** 
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <iostream>
+#include "php_module.hpp"
 
+/*
 int main(int argc, char **argv, char **env)
 {
     int pipefd[2];
@@ -69,4 +65,90 @@ int main(int argc, char **argv, char **env)
         std::cout << "le content final: " << std::endl << content << std::endl;
     }
     return (0);
+}
+*/
+
+OPEN_ZIA_MAKE_ENTRY_POINT(PHP_CGI)
+
+void PHP_CGI::onRegisterCallbacks(oZ::Pipeline &pipeline)
+{
+    std::cout << "=> PHP_CGI" << std::endl;
+    pipeline.registerCallback(
+        oZ::State::Interpret,
+        oZ::Priority::ASAP,
+        this, &Fill::takeContent
+    );
+}
+
+bool PHP_CGI::execPHP(oZ::Context &context)
+{
+    int pipefd[2];
+    pid_t pid;
+    char buf;
+
+    if (findPHP(context) == false)
+        return true;
+    if (pipe(pipefd) == -1)
+        return true;
+    pid = fork();
+    if (pid == -1)
+        return true;
+    const char *bin = "/usr/bin/php-cgi";
+    const char *argv[] = {bin, NULL};    // commande pour lancer l'interpréteur
+    char *tmpPath = "tmp/phpExecTmp.XXXXXX";
+    int fd;
+    
+    if ((fd = mkstemp(tmpPath)) == -1)
+        return true;
+    write(fd, context.getResponse().getBody().c_str(), context.getResponse().getBody().size());
+    char *tmpArgv[] = { (char *)"/usr/bin/php-cgi", tmpPath, NULL };
+    if (pid == 0) {                             // Le fils
+        close(pipefd[0]);
+        if (dup2(pipefd[1], 1) == -1)
+            exit(84);
+        int rc = execve(tmpArgv[0], &tmpArgv[0], nullptr);
+        perror("execve");
+        _exit(rc);
+    } else {
+        close(pipefd[1]);
+        int exitCode = 0;
+        waitpid(pid, &exitCode, 0);
+        if (exitCode != 0) {
+            std::cout << "PHP Module: execve fail" << std::endl;
+            return true;
+        }
+        unlink(tmpPath);
+        close(fd);
+        int r = -1;
+        char buff[128];
+        std::string content;
+        while ((r = read(pipefd[0], buff, 128)) != -1 && r > 0)
+            content.append(buff, r);
+        close(pipefd[0]);
+        if (r == -1)
+            return true;
+        content = content.substr(content.find("<"));
+        context.getResponse().getBody() = content;
+        context.getResponse().getHeader().set("Content-Length", std::to_string(content.size()));
+        return true;
+    }
+}
+
+bool PHP_CGI::findPHP(const oZ::Context &context)
+{
+    std::string path(HTML_FILES_POSI + PHPModule::routes_enums[context.getRequest().getURI()]);
+    std::ifstream is(path.c_str(), std::ios::in | std::ios::binary);
+    char buf[512];
+    std::string content;
+
+    if (!is.is_open()) {
+        std::cout << "PHP: Can't open the file" << std::endl;
+        return false;
+    }
+    while (is.read(buf, sizeof(buf)).gcount() > 0)
+        content.append(buf, is.gcount());
+    is.close();
+    if (content.find("<?php") != std::string::npos)
+        return true;
+    return false;
 }
---
+++ src/modules/php_cgi/php_module.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2020
+** CPP_zia_2019
+** File description:
+** PHP
+*/
+
+#pragma once
+
+#include <openZia/IModule.hpp>
+#include <openZia/Pipeline.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <fstream>
+#include <map>
+
+#define HTML_FILES_POSI "src/HTML/"
+
+namespace PHPModule
+{
+    std::map<std::string, std::string> routes_enums = {
+        {"/",      "index.html"},
+        {"/test",  "index.html"},
+        {"/php",   "index_php.html"}
+    };
+}
+
+class PHP_CGI : public oZ::IModule 
+{
+    public:
+        PHP_CGI() = default;
+
+        virtual const char *getName(void) const { return "PHPModule"; }
+
+        virtual void onRegisterCallbacks(oZ::Pipeline &pipeline);
+
+    private:
+        bool execPHP(oZ::Context &context);
+        bool findPHP(const oZ::Context &context);
+
+};
