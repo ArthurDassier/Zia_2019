2b8b2ce0fcd7101dc0712779f2d19055d521dc35
Camille Bouzerand <camille.bouzerand@epitech.eu>
2020-02-24 21:54:27 +0100

    add php module izi game izi life

--- src/modules/php_cgi/php_module.cpp
+++ src/modules/php_cgi/php_module.cpp
@@ -7,76 +7,15 @@
 
 #include "php_module.hpp"
 
-/*
-int main(int argc, char **argv, char **env)
-{
-    int pipefd[2];
-    pid_t pid;
-    char buf;
-
-    if (pipe(pipefd) == -1) {
-        perror("pipe");
-        exit(EXIT_FAILURE);
-    }
-
-    pid = fork();
-    if (pid == -1) {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
-
-    if (pid == 0) {                             // Le fils
-        const char *bin = "/usr/bin/php-cgi";
-        // const char *argv[] = {bin, NULL};    // commande pour lancer l'interpréteur
-        close(pipefd[0]);                       // Ferme l'extrémité de lecture inutilisée
-        if (dup2(pipefd[1], 1) == -1) {
-            std::cout << "dup2 a echoue" << std::endl;
-            exit(84);
-        }
-
-        //-f ou rien c un fichier, -r il faut exec la ligne direct
-        int rc = execve(
-            bin,
-            const_cast<char * const *>(argv),
-            const_cast<char * const *>(env)
-            );
-
-        _exit(rc);
-
-    } else {
-        close(pipefd[1]);
-        int exitCode = 0;
-        waitpid(pid, &exitCode, 0);
-        if (exitCode != 0) {
-            std::cout << "hum l'execve a pas marché" << std::endl;
-            exit(84);
-        }
-
-        int r = -1;
-        char buff[128];
-        std::string content;
-        while ((r = read(pipefd[0], buff, 128)) != -1 && r > 0)
-            content.append(buff, r);
-        close(pipefd[0]);
-        if (r == -1) {
-            std::cout << "hum pb de read" << std::endl;
-            exit(84);
-        }
-        std::cout << "le content final: " << std::endl << content << std::endl;
-    }
-    return (0);
-}
-*/
-
 OPEN_ZIA_MAKE_ENTRY_POINT(PHP_CGI)
 
 void PHP_CGI::onRegisterCallbacks(oZ::Pipeline &pipeline)
 {
     std::cout << "=> PHP_CGI" << std::endl;
     pipeline.registerCallback(
-        oZ::State::Interpret,
+        oZ::State::AfterInterpret,
         oZ::Priority::ASAP,
-        this, &Fill::takeContent
+        this, &PHP_CGI::execPHP
     );
 }
 
@@ -84,54 +23,60 @@ bool PHP_CGI::execPHP(oZ::Context &context)
 {
     int pipefd[2];
     pid_t pid;
-    char buf;
 
-    if (findPHP(context) == false)
+    if (!findPHP(context))
         return true;
-    if (pipe(pipefd) == -1)
+    if (pipe(pipefd) == -1) {
+        std::cout << "PHPModule: error in pipe" << std::endl;
         return true;
+    }
     pid = fork();
-    if (pid == -1)
+    if (pid == -1) {
+        std::cout << "PHPModule: error in fork" << std::endl;
         return true;
-    const char *bin = "/usr/bin/php-cgi";
-    const char *argv[] = {bin, NULL};    // commande pour lancer l'interpréteur
-    char *tmpPath = "tmp/phpExecTmp.XXXXXX";
+    }
+
+    char tmpPath[] = "/tmp/tmpPhpExec.XXXXXX";
     int fd;
-    
-    if ((fd = mkstemp(tmpPath)) == -1)
+    if ((fd = mkstemp(tmpPath)) == -1) {
+        std::cout << "PHPModule: error in mkstemp" << std::endl;
         return true;
+    }
     write(fd, context.getResponse().getBody().c_str(), context.getResponse().getBody().size());
-    char *tmpArgv[] = { (char *)"/usr/bin/php-cgi", tmpPath, NULL };
-    if (pid == 0) {                             // Le fils
+
+    char *tmpArgv[] = { (char *)"/usr/bin/php-cgi", (char *)tmpPath, NULL };
+    if (pid == 0) {
         close(pipefd[0]);
         if (dup2(pipefd[1], 1) == -1)
             exit(84);
         int rc = execve(tmpArgv[0], &tmpArgv[0], nullptr);
-        perror("execve");
         _exit(rc);
     } else {
         close(pipefd[1]);
         int exitCode = 0;
         waitpid(pid, &exitCode, 0);
         if (exitCode != 0) {
-            std::cout << "PHP Module: execve fail" << std::endl;
+            std::cout << "PHPModule: error in execve" << std::endl;
             return true;
         }
-        unlink(tmpPath);
         close(fd);
+        unlink(tmpPath);
         int r = -1;
         char buff[128];
         std::string content;
         while ((r = read(pipefd[0], buff, 128)) != -1 && r > 0)
             content.append(buff, r);
         close(pipefd[0]);
-        if (r == -1)
+        if (r == -1) {
+            std::cout << "PHPModule: can't read the result" << std::endl;
             return true;
+        }
         content = content.substr(content.find("<"));
         context.getResponse().getBody() = content;
-        context.getResponse().getHeader().set("Content-Length", std::to_string(content.size()));
+        context.getResponse().getHeader().get("Content-Length") = content.size();
         return true;
     }
+    return true;
 }
 
 bool PHP_CGI::findPHP(const oZ::Context &context)
--- src/modules/php_cgi/php_module.hpp
+++ src/modules/php_cgi/php_module.hpp
@@ -14,6 +14,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 #include <iostream>
 #include <fstream>
 #include <map>
