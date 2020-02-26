610fc61dda1404c9fdc6ec864d633e955a2e798b
Camille Bouzerand <camille.bouzerand@epitech.eu>
2020-02-26 21:37:28 +0100

    [FIX] Management of netcat ping error

--- src/server/Connection.cpp
+++ src/server/Connection.cpp
@@ -91,7 +91,7 @@ void Zia::Connection::send(oZ::Context &&context)
         + context.getResponse().getBody()
     );
 
-    std::cout << "~~packet: " << std::endl << response << std::endl;
+    // std::cout << "~~packet: " << std::endl << response << std::endl;
 
     auto self(shared_from_this());
     boost::asio::async_write(_socket, boost::asio::buffer(response),
@@ -101,7 +101,6 @@ void Zia::Connection::send(oZ::Context &&context)
             _connectionManager.eraseClient(shared_from_this());
             return;
         }
-        std::cout << "jai fini de send" << std::endl;
         read();
         // boost::system::error_code err;
         // _socket.shutdown(socket::shutdown_both, err);
